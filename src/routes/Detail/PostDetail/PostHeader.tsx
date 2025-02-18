8651f724b2beb41dae99a1d9843935344970d983
Rappire <65114744+rappire@users.noreply.github.com>
2025-02-19 01:40:53 +0900

    Update PostHeader.tsx

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -20,13 +20,13 @@ const PostHeader: React.FC<Props> = ({ data }) => {
             {data.author && data.author[0] && data.author[0].name && (
               <>
                 <div className="author">
-                  <Image
+{/*                   <Image
                     css={{ borderRadius: "50%" }}
                     src={data.author[0].profile_photo || CONFIG.profile.image}
                     alt="profile_photo"
                     width={24}
                     height={24}
-                  />
+                  /> */}
                   <div className="">{data.author[0].name}</div>
                 </div>
                 <div className="hr"></div>
